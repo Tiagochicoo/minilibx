2c4351d1423363db4cffc0e71b97d2498598d6f0
tpereira <tpereira@student.42lisboa.com>
2022-04-08 18:03:58 +0100

    save

---
+++ includes/fdf.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 16:21:52 by tpereira          #+#    #+#             */
+/*   Updated: 2022/04/08 17:50:23 by tpereira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+#include "../libft/includes/libft.h"
+#include <mlx.h>
+
+typedef struct	s_data {
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct 
+{
+	int	width;
+	int	height;
+	int	**z_matrix;
+
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				fdf;
+
+void	read_file(char *filename, fdf *data);
+void	draw_square(t_data*img, int x, int y, unsigned long color);
+
+#endif
